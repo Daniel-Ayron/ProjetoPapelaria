03f0c25584f234df291f93cd970535b77e7c1f39
DANIEL AYRON SILVA DE OLIVEIRA <daniel.oliveira161@senaisp.edu.br>
2026-08-28 16:54:24 -0300

    criando superusuario e cofigurando outras coisas

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nome  = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
+    ativa = models.BooleanField(default=True)
+
+    def __string__(self):
+        return self.nome
