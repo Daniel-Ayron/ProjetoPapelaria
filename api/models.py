6dcf7698936230ee12bdda0a6f5064f84f507423
DANIEL AYRON SILVA DE OLIVEIRA <daniel.oliveira161@senaisp.edu.br>
2026-09-11 16:04:32 -0300

    adicionando mais um usuário e dando migrate

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Categoria(models.Model):
     nome  = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=255)
+    descricao = models.CharField()
     ativa = models.BooleanField(default=True)
 
     def __string__(self):
